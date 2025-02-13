c403365bdcbdff36dc59cd0af60b3aa34fbddf3b
lachy <lachlan.ho@student.unsw.edu.au>
2025-02-13 15:33:06 +1100

    created hero page

---
+++ js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const particlesContainer = document.getElementById('particles-js');
+  const particlesCount = 150;
+
+  let particleIndex = 0
+  const createParticle = () => {
+    if (particleIndex < particlesCount) {
+      const particle = document.createElement('div');
+      particle.classList.add('particle');
+      particle.style.left = `${Math.random() * 100}vw`;
+      particle.style.top = `${Math.random() * 50 + 8}vh`;
+      particle.style.animationDelay = `${Math.random() * 5}s, ${Math.random() * 2}s`;
+      particlesContainer.appendChild(particle);
+
+      particleIndex++;
+      requestAnimationFrame(createParticle);
+    }
+  };
+
+  requestAnimationFrame(createParticle);
+});
+
